2d1b92371283f6706af32014fe81edd8cda10cc9
Emerald <emeraldarista@Emeralds-MacBook-Air.local>
2026-03-17 16:07:22 +1100

    test terraform commands in gh actions

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,16 +22,16 @@ resource "aws_internet_gateway" "ig" {
 
 resource "aws_route_table" "route_table" {
   vpc_id = aws_vpc.main.id
-  
+
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.ig.id
   }
 
   route {
-    ipv6_cidr_block        = "::/0"
-    gateway_id = aws_internet_gateway.ig.id
-  }  
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.ig.id
+  }
 
   route {
     cidr_block = aws_subnet.public_subnet.cidr_block
@@ -97,48 +97,16 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
 
 # create ec2
 resource "aws_instance" "ec2" {
-  ami           = var.ami
-  instance_type = var.instance_type
+  ami                         = var.ami
+  instance_type               = var.instance_type
   subnet_id                   = aws_subnet.public_subnet.id
   vpc_security_group_ids      = [aws_security_group.allow_tls.id]
   associate_public_ip_address = true
-  key_name = "local-keypair"
+  key_name                    = "local-keypair"
 
   tags = {
     Name = "flask-app-instance"
   }
 
-  user_data = <<-EOF
-      #!/bin/bash
-      sudo apt update -y
-      sudo apt install -y apache2 openssl
-
-      sudo systemctl start apache2
-      sudo systemctl enable apache2
-
-      sudo bash -c "echo your very first server > /var/www/html/index.html"
-
-      # Enable SSL module
-      sudo a2enmod ssl
-
-      # Generate self-signed certificate
-      sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-        -keyout /etc/ssl/private/apache-selfsigned.key \
-        -out /etc/ssl/certs/apache-selfsigned.crt \
-        -subj "/C=US/ST=State/L=City/O=Org/CN=localhost"
-
-      # Configure SSL virtual host
-      sudo bash -c 'cat > /etc/apache2/sites-available/default-ssl.conf <<SSLCONF
-      <VirtualHost *:443>
-        ServerName localhost
-        DocumentRoot /var/www/html
-        SSLEngine on
-        SSLCertificateFile /etc/ssl/certs/apache-selfsigned.crt
-        SSLCertificateKeyFile /etc/ssl/private/apache-selfsigned.key
-      </VirtualHost>
-      SSLCONF'
-
-      sudo a2ensite default-ssl
-      sudo systemctl restart apache2
-      EOF
+  user_data = file("${path.module}/user_data.sh")
 }
---
+++ terraform/user_data.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt install -y apache2 openssl
+
+sudo systemctl start apache2
+sudo systemctl enable apache2
+
+sudo bash -c "echo your very first server > /var/www/html/index.html"
+
+# Enable SSL module
+sudo a2enmod ssl
+
+# Generate self-signed certificate
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+  -keyout /etc/ssl/private/apache-selfsigned.key \
+  -out /etc/ssl/certs/apache-selfsigned.crt \
+  -subj "/C=US/ST=State/L=City/O=Org/CN=localhost"
+
+# Configure SSL virtual host
+sudo bash -c 'cat > /etc/apache2/sites-available/default-ssl.conf <<SSLCONF
+<VirtualHost *:443>
+  ServerName localhost
+  DocumentRoot /var/www/html
+  SSLEngine on
+  SSLCertificateFile /etc/ssl/certs/apache-selfsigned.crt
+  SSLCertificateKeyFile /etc/ssl/private/apache-selfsigned.key
+</VirtualHost>
+SSLCONF'
+
+sudo a2ensite default-ssl
+sudo systemctl restart apache2
